91ae67a5167316f0f7a3841b8da4a9b89363186d
KauBoui <errolbucy@gmail.com>
2023-05-09 12:30:47 -0700

    I spelled view wrong....

--- docs/make.jl
+++ docs/make.jl
@@ -4,5 +4,5 @@ using FractalAnimation
 makedocs(sitename="FractalAnimation.jl")
 
 deploydocs(
-    repo = "github.com/ErrolSeders/FractalAnimation.jl.git", push_preveiw=true 
+    repo = "github.com/ErrolSeders/FractalAnimation.jl.git", push_preview=true 
 )
